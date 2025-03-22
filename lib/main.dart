cf91f5b590c9a1de3e81a4817ae00f061625d74e
mhq1368 <qassabi.mh@gmail.com>
2025-03-22 10:44:23 +0330

    Refactor main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:music_player_app/1.dart';
 import 'package:music_player_app/Controllers/theme_controller.dart';
 import 'package:music_player_app/Views/home_screen_views.dart';
 
